312e02b84d79e8fe6c8cf176704d4e11f65cd605
Mattia Gallinaro <mgallinaro@remedia.it>
2019-08-26 09:31:44 +0200

    Responsive charts.

--- src/components/dash/components/chart/index.js
+++ src/components/dash/components/chart/index.js
@@ -10,7 +10,18 @@ import { LinearGradient } from '@vx/gradient';
 import { extent, max } from 'd3-array';
 
 //recharts import
-import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area } from 'recharts';
+import { 
+  AreaChart, 
+  XAxis, 
+  YAxis, 
+  CartesianGrid, 
+  Tooltip, 
+  Area ,
+  ResponsiveContainer,
+  ReferenceLine
+} from 'recharts';
+
+import './style.scss';
 
 const data = appleStock.slice(800);
 
@@ -57,7 +68,52 @@ const secondData = [
     "pv": 4300,
     "amt": 2100
   }
-]
+];
+
+const thirdData = [
+  {
+    "name": "Page A",
+    "uv": 4000,
+    "pv": 2400,
+    "amt": 2400
+  },
+  {
+    "name": "Page B",
+    "uv": 3000,
+    "pv": 1398,
+    "amt": 2210
+  },
+  {
+    "name": "Page C",
+    "uv": 2000,
+    "pv": 9800,
+    "amt": 2290
+  },
+  {
+    "name": "Page D",
+    "uv": 2780,
+    "pv": 3908,
+    "amt": 2000
+  },
+  {
+    "name": "Page E",
+    "uv": 1890,
+    "pv": 4800,
+    "amt": 2181
+  },
+  {
+    "name": "Page F",
+    "uv": 2390,
+    "pv": 3800,
+    "amt": 2500
+  },
+  {
+    "name": "Page G",
+    "uv": 3490,
+    "pv": 4300,
+    "amt": 2100
+  }
+];
 
 const width = 750;
 const height = 400;
@@ -146,5 +202,19 @@ export default () => (
       <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
     </AreaChart>
   </div>
+    <div className={ 'chart-container' }>
+      <ResponsiveContainer width="50%" aspect={ 3 }>
+        <AreaChart data={thirdData}
+          margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <ReferenceLine x="Page C" stroke="green" label="Min PAGE" />
+          <ReferenceLine y={4000} label="Max" stroke="red" strokeDasharray="3 3" />
+          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
   </div>
 )
